c4bc002e6a94310d367c17825be12a1b19fa814e
Jin Pan <jinpan@mit.edu>
2014-01-01 01:35:53 -0500

    updated admin site login page

--- testserver/urls.py
+++ testserver/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
 from testserver.core.urls import urlpatterns as core_patterns
 
 
 admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
