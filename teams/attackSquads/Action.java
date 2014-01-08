0a558037ec74c2a856d8bdc0fe20e95da6e607b9
vye1116 <vye1116@gmail.com>
2014-01-08 17:13:49 -0500

    very buggy. will merge with other versions

---
+++ teams/attackSquads/Action.java
@@ -0,0 +1,18 @@
+package jinpan;
+
+import jinpan.BaseRobot.State;
+import battlecode.common.MapLocation;
+
+public class Action {
+	
+	public BaseRobot.State myState;
+	public MapLocation targetLocation;
+	public int targetID;
+	
+	public Action(BaseRobot.State state, MapLocation target, int targetID){
+		this.myState = state;
+		this.targetLocation = target;
+		this.targetID = targetID;
+	}
+
+}
---
+++ teams/attackSquads/Message.java
@@ -0,0 +1,10 @@
+package jinpan;
+
+public interface Message {
+    
+    public int encode();
+    
+    // TODO: figure out how to deal with static methods here
+    // public static Message decode(int message);
+
+}
