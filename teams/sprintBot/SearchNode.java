9346f4ee012a0b6663e9c64886c2c52ac934d2de
Deniz Oktay <denizoktay1994@gmail.com>
2014-01-13 17:33:25 -0500

    Added searchnode

---
+++ teams/sprintBot/SearchNode.java
@@ -0,0 +1,20 @@
+package sprintBot;
+import java.util.LinkedList;
+
+import battlecode.common.MapLocation;
+
+public class SearchNode {
+	public SearchNode prevLoc;
+	public MapLocation loc;
+	public int length;
+	public boolean isPivot = false;
+	public SearchNode(MapLocation location, int length, SearchNode prevLoc, BaseRobot br){
+		this.loc = location;
+		this.prevLoc = prevLoc;
+		this.length = length;
+	}
+	@Override
+	public boolean equals(Object otherSN) {
+		return (this.loc.x == ((SearchNode) otherSN).loc.x) && (this.loc.y == ((SearchNode) otherSN).loc.y);
+	}
+}
