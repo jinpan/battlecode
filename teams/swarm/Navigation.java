953afd3c92fdd26e9a03eb35c2c941ceb7c58934
Deniz Oktay <denizoktay1994@gmail.com>
2014-01-23 19:16:24 -0500

    nom navigation

--- teams/swarm/Navigation.java
+++ teams/swarm/Navigation.java
@@ -20,11 +20,11 @@ public class Navigation {
 		int closestLeft = curr.distanceSquaredTo(ehqloc);
 		// optimize, kinda slow right now. -> lol no it's good enough.
 		// Initialize mline array for easy lookup later
-		// while (!(curr.x == ehqloc.x && curr.y == ehqloc.y)) {
-		// mline[curr.x][curr.y] = true;
-		// curr = curr.add(curr.directionTo(ehqloc));
-		// System.out.println(curr);
-		// }
+//		while (!(curr.x == ehqloc.x && curr.y == ehqloc.y)) {
+//			mline[curr.x][curr.y] = true;
+//			curr = curr.add(curr.directionTo(ehqloc));
+//			System.out.println(curr);
+//		}
 		// Actual bug iteration
 		SearchNode current = new SearchNode(start, 1, null, thisBot);
 		SearchNode currentLeft = new SearchNode(start, 1, null, thisBot);
@@ -41,6 +41,7 @@ public class Navigation {
 					thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.ROAD);
 			boolean canMoveForwardLeft = (thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.NORMAL ||
 					thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.ROAD);
+			
 			// If on the m-line, and can move forward, move forward towards the enemy HQ.
 			if (directionTo(current.loc, ehqloc) != null && current.loc.distanceSquaredTo(ehqloc) <= closestRight) {
 				if (debug) System.out.println("right step 1");
@@ -82,10 +83,10 @@ public class Navigation {
 						thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.ROAD)) {
 					curDir = curDir.rotateLeft();
 				}
-				// if (!(thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.NORMAL ||
-				// thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.ROAD)) {
-				// curDir = curDir.rotateLeft();
-				// }
+//				if (!(thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.NORMAL ||
+//						thisBot.myRC.senseTerrainTile(current.loc.add(curDir)) == TerrainTile.ROAD)) {
+//					curDir = curDir.rotateLeft();
+//				}
 				if (thisBot.myRC.senseTerrainTile(current.loc.add(curDir.rotateRight().rotateRight().rotateRight().rotateRight())) == TerrainTile.VOID) {
 					//System.out.println("Corner found at " + current.loc);
 				}
@@ -133,10 +134,10 @@ public class Navigation {
 						thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.ROAD)) {
 					curDirLeft = curDirLeft.rotateRight();
 				}
-				// if (!(thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.NORMAL ||
-				// thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.ROAD)) {
-				// curDirLeft = curDirLeft.rotateRight();
-				// }
+//				if (!(thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.NORMAL ||
+//						thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft)) == TerrainTile.ROAD)) {
+//					curDirLeft = curDirLeft.rotateRight();
+//				}
 				if (thisBot.myRC.senseTerrainTile(currentLeft.loc.add(curDirLeft.rotateLeft().rotateLeft().rotateLeft().rotateLeft())) == TerrainTile.VOID) {
 					//System.out.println("Corner found at " + currentLeft.loc);
 				}
@@ -154,10 +155,11 @@ public class Navigation {
 			System.out.println("Left bug");
 			return currentLeft;
 		}
+		
 	}
 	public static LinkedList<MapLocation> pathFind(MapLocation start, MapLocation target, BaseRobot theBot) throws GameActionException {
 		thisBot = theBot;
-		SearchNode bugSearch = bugSearch(start, target, theBot);
+		SearchNode bugSearch = bugSearchNew(start, target, theBot);
 		SearchNode[] nodes = new SearchNode[bugSearch.length];
 		int counter = bugSearch.length-1;
 		while (!bugSearch.loc.equals(start)) {
@@ -210,40 +212,153 @@ public class Navigation {
 		}
 		return true;
 	}
+	
 	protected static Direction directionTo(MapLocation curr, MapLocation loc) throws GameActionException {
-		// Direction dir = curr.directionTo(loc);
-		//
-		// if (thisBot.myRC.senseTerrainTile(curr.add(dir)) == TerrainTile.NORMAL ||
-		// thisBot.myRC.senseTerrainTile(curr.add(dir)) == TerrainTile.ROAD){
-		// return dir;
-		// }
-		//
-		// Direction dirA, dirB;
-		// if (thisBot.random() < 0.5){
-		// dirA = dir.rotateLeft();
-		// dirB = dir.rotateRight();
-		// }
-		// else {
-		// dirA = dir.rotateRight();
-		// dirB = dir.rotateLeft();
-		// }
-		//
-		// if (thisBot.myRC.senseTerrainTile(curr.add(dirA)) == TerrainTile.NORMAL ||
-		// thisBot.myRC.senseTerrainTile(curr.add(dirA)) == TerrainTile.ROAD){
-		// return dirA;
-		// }
-		// else if (thisBot.myRC.senseTerrainTile(curr.add(dirB)) == TerrainTile.NORMAL ||
-		// thisBot.myRC.senseTerrainTile(curr.add(dirB)) == TerrainTile.ROAD){
-		// return dirB;
-		// }
-		//
-		// return null;
+//		Direction dir = curr.directionTo(loc);
+//
+//		if (thisBot.myRC.senseTerrainTile(curr.add(dir)) == TerrainTile.NORMAL ||
+//				thisBot.myRC.senseTerrainTile(curr.add(dir)) == TerrainTile.ROAD){
+//			return dir;
+//		}
+//
+//		Direction dirA, dirB;
+//		if (thisBot.random() < 0.5){
+//			dirA = dir.rotateLeft();
+//			dirB = dir.rotateRight();
+//		}
+//		else {
+//			dirA = dir.rotateRight();
+//			dirB = dir.rotateLeft();
+//		}
+//
+//		if (thisBot.myRC.senseTerrainTile(curr.add(dirA)) == TerrainTile.NORMAL ||
+//				thisBot.myRC.senseTerrainTile(curr.add(dirA)) == TerrainTile.ROAD){
+//			return dirA;
+//		}
+//		else if (thisBot.myRC.senseTerrainTile(curr.add(dirB)) == TerrainTile.NORMAL ||
+//				thisBot.myRC.senseTerrainTile(curr.add(dirB)) == TerrainTile.ROAD){
+//			return dirB;
+//		}
+//
+//		return null;        
 		if (thisBot.myRC.senseTerrainTile(curr.add(curr.directionTo(loc))) == TerrainTile.NORMAL ||
-				thisBot.myRC.senseTerrainTile(curr.add(curr.directionTo(loc))) == TerrainTile.ROAD)
+					thisBot.myRC.senseTerrainTile(curr.add(curr.directionTo(loc))) == TerrainTile.ROAD)
 			return curr.directionTo(loc);
 		else
 			return null;
-
 	}
-
+	public static SearchNode bugSearchNew(MapLocation start, MapLocation target, BaseRobot theBot) throws GameActionException{
+		System.out.println("CALCULATING PATH FROM " + start + " to " + target);
+		thisBot = theBot;
+		boolean debug = true;
+		MapLocation ehqloc = target;
+		MapLocation curr = start;
+		int closestRight = curr.distanceSquaredTo(ehqloc);
+		int closestLeft = curr.distanceSquaredTo(ehqloc);
+		SearchNode current = new SearchNode(start, 1, null, thisBot);
+		SearchNode currentLeft = new SearchNode(start, 1, null, thisBot);
+		current.isPivot = true;
+		currentLeft.isPivot = true;
+		boolean isTracing = false;
+		boolean isTracingLeft = false;
+		Direction curDir = current.loc.directionTo(ehqloc);
+		Direction curDirLeft = current.loc.directionTo(ehqloc);
+		while (!(current.loc.x == ehqloc.x && current.loc.y == ehqloc.y) && !(currentLeft.loc.x == ehqloc.x && currentLeft.loc.y == ehqloc.y)) {
+			if (debug) System.out.println("Right " + current.loc + " Left " + currentLeft.loc);
+			if (!isTracing) {
+				curDir = directionTo(current.loc, ehqloc);
+				if (curDir != null /* && current.loc.distanceSquaredTo(ehqloc) <= closestRight*/) {
+					if (debug) System.out.println("right step 1");
+					current = new SearchNode(current.loc.add(curDir), current.length+1, current, thisBot);
+					if (current.loc.distanceSquaredTo(ehqloc) < closestRight)
+						closestRight = current.loc.distanceSquaredTo(ehqloc);
+				} else {
+					isTracing = true;
+					curDir = current.loc.directionTo(ehqloc);
+					while (!isGood(current.loc.add(curDir))) {
+						curDir = curDir.rotateLeft();
+					}
+					if (debug) System.out.println("right step 2");
+					current = new SearchNode(current.loc.add(curDir), current.length+1, current, thisBot);
+					if (current.loc.distanceSquaredTo(ehqloc) < closestRight)
+						closestRight = current.loc.distanceSquaredTo(ehqloc);
+				}
+			} else {
+				curDir = directionTo(current.loc, ehqloc);
+				if (curDir != null && current.loc.add(curDir).distanceSquaredTo(ehqloc) < closestRight) {
+					isTracing = false;
+					System.out.println("Not tracing anymore. Distance of " + current.loc.add(curDir).distanceSquaredTo(ehqloc));
+				} else {
+					curDir = current.loc.directionTo(current.prevLoc.loc).rotateLeft().rotateLeft();
+					int i = 2;
+					while (!isGood(current.loc.add(curDir))) {
+						curDir = curDir.rotateLeft();
+						i++;
+					}
+					if (i < 4 || curDir != Direction.EAST && curDir != Direction.WEST && curDir != Direction.NORTH && curDir != Direction.SOUTH) {
+						current.isPivot = true;
+					}
+					if (debug) System.out.println("right step 3");
+					current = new SearchNode(current.loc.add(curDir), current.length+1, current, thisBot);
+					if (current.loc.distanceSquaredTo(ehqloc) < closestRight)
+						closestRight = current.loc.distanceSquaredTo(ehqloc);
+				}
+			}
+			
+			if (debug) System.out.println(currentLeft.loc);
+			if (!isTracingLeft) {
+				curDirLeft = directionTo(currentLeft.loc, ehqloc);
+				if (curDirLeft != null /* && current.loc.distanceSquaredTo(ehqloc) <= closestRight*/) {
+					if (debug) System.out.println("left step 1");
+					currentLeft = new SearchNode(currentLeft.loc.add(curDirLeft), currentLeft.length+1, currentLeft, thisBot);
+					if (currentLeft.loc.distanceSquaredTo(ehqloc) < closestLeft)
+						closestLeft = currentLeft.loc.distanceSquaredTo(ehqloc);
+				} else {
+					isTracingLeft = true;
+					curDirLeft = currentLeft.loc.directionTo(ehqloc);
+					while (!isGood(currentLeft.loc.add(curDirLeft))) {
+						curDirLeft = curDirLeft.rotateRight();
+					}
+					if (debug) System.out.println("left step 2");
+					currentLeft = new SearchNode(currentLeft.loc.add(curDirLeft), currentLeft.length+1, currentLeft, thisBot);
+					if (currentLeft.loc.distanceSquaredTo(ehqloc) < closestLeft)
+						closestLeft = currentLeft.loc.distanceSquaredTo(ehqloc);
+				}
+			} else {
+				curDirLeft = directionTo(currentLeft.loc, ehqloc);
+				if (curDirLeft != null && currentLeft.loc.add(curDirLeft).distanceSquaredTo(ehqloc) < closestLeft) {
+					isTracingLeft = false;
+					System.out.println("Not tracing anymore. Distance of " + currentLeft.loc.add(curDirLeft).distanceSquaredTo(ehqloc));
+				} else {
+					curDirLeft = currentLeft.loc.directionTo(currentLeft.prevLoc.loc).rotateRight().rotateRight();
+					int j = 2;
+					while (!isGood(currentLeft.loc.add(curDirLeft))) {
+						curDirLeft = curDirLeft.rotateRight();
+						j++;
+					}
+					if (j < 4 || curDirLeft != Direction.EAST && curDirLeft != Direction.WEST && curDirLeft != Direction.NORTH && curDirLeft != Direction.SOUTH) {
+						currentLeft.isPivot = true;
+					}
+					if (debug) System.out.println("left step 3");
+					currentLeft = new SearchNode(currentLeft.loc.add(curDirLeft), currentLeft.length+1, currentLeft, thisBot);
+					if (currentLeft.loc.distanceSquaredTo(ehqloc) < closestLeft)
+						closestLeft = currentLeft.loc.distanceSquaredTo(ehqloc);
+				}
+			}
+		}
+		currentLeft.isPivot = true;
+		current.isPivot = true;
+		if (current.loc.equals(ehqloc)) {
+			System.out.println("Right bug");
+			return current;
+		} else {
+			System.out.println("Left bug");
+			return currentLeft;
+		}
+	}
+	
+	public static boolean isGood(MapLocation loc) {
+		return (thisBot.myRC.senseTerrainTile(loc) == TerrainTile.NORMAL ||
+				thisBot.myRC.senseTerrainTile(loc) == TerrainTile.ROAD);
+	}
 }
